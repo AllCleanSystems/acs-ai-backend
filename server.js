82be5eccd85bc7652f1ce0f296c8c9ff8efedfd1
AllCleanSystems <Veal.joe@yahoo.com>
2026-04-21 10:44:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fetch = require("node-fetch");
 const OpenAI = require("openai");
 const twilio = require("twilio");
 const crypto = require("crypto");
+const adminAuthRoutes = require('./routes/admin-auth');
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(cors());
 // Twilio sends webhooks as application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const adminAuthRoutes = require('./routes/admin-auth');
 
 const openai = process.env.OPENAI_API_KEY
   ? new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
